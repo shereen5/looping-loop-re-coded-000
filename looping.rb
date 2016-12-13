7e2b01c563af5e15706beeb7bc5254b9e51884f6
shereen messi <shereenmessi@hotmail.com>
2016-12-13 16:44:36 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,14 @@
 def looping
 	#your code here
+  counter=1
+  loop do
+    puts "Wingardium Leviosa"
+    counter+=1
+    if counter==10
+      break
+    end
+  end
 end
 
 #call your method here
+looping
